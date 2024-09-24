82e252c5f383d1f2a1faf60ab2229307e4411be9
Sean W. <104464288+swilso59@users.noreply.github.com>
2024-09-24 14:30:18 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,9 +2,9 @@ function fib(n) {
     if (n === 0) {
         return [0];
     } else if (n === 1) {
-        return [0,1];
+        return [0, 1];
     } else {
-        var arr = fib(n -1);
+        var arr = fib(n - 1);
         arr.push(arr[n - 2] + arr[n - 1]);
         return arr;
     } 
